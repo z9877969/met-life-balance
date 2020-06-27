50d9b3268043a466abd59448112e2707e1e11577
Andriy Kyrylenko <z9877969@ukr.net>
2020-06-27 22:23:25 +0300

    Updates

---
+++ precache-manifest.5a8d01127ca09c610ce291d7e979eede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99914d399630d2bc4304a84ef644886",
+    "url": "/met-life-balance/index.html"
+  },
+  {
+    "revision": "68a23e07971cb94e713e",
+    "url": "/met-life-balance/static/css/main.451906db.chunk.css"
+  },
+  {
+    "revision": "d6b1b63f5e8580b9f0dd",
+    "url": "/met-life-balance/static/js/2.7836394d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/met-life-balance/static/js/2.7836394d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a23e07971cb94e713e",
+    "url": "/met-life-balance/static/js/main.dec676c1.chunk.js"
+  },
+  {
+    "revision": "2c117c6abac83c46f5cd",
+    "url": "/met-life-balance/static/js/runtime-main.48f10c21.js"
+  }
+]);
